791c4231cba9fbd1e3deb418e6d18d687dbbe97b
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2026-09-07 18:13:35 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,10 @@ repositories {
 val lombokVersion = "1.18.42"
 
 dependencies {
-    implementation("com.mysql:mysql-connector-j:8.3.0")
+    implementation("com.mysql:mysql-connector-j:8.4.0")
     implementation("org.flywaydb:flyway-core:10.11.1")
-    implementation("org.flywaydb:flyway-mysql:10.11.1")
+    implementation("org.flywaydb:flyway-mysql:10.12.0")
+    implementation("net.datafaker:datafaker:2.2.2")
 
     compileOnly("org.projectlombok:lombok:$lombokVersion")
     annotationProcessor("org.projectlombok:lombok:$lombokVersion")
