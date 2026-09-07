a0952fa0e05055469b79e9209772c3c04930305a
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2026-09-07 18:15:01 -0300

    feat: adiciona gerando usuário para o banco de dados

--- src/main/java/br/com/dio/Main.java
+++ src/main/java/br/com/dio/Main.java
@@ -6,16 +6,24 @@ import br.com.dio.persistence.EmployeeAuditDAO;
 import br.com.dio.persistence.EmployeeDAO;
 import br.com.dio.persistence.EmployeeParamDAO;
 import br.com.dio.persistence.entity.EmployeeEntity;
+import net.datafaker.Faker;
 import org.flywaydb.core.Flyway;
 
 import java.math.BigDecimal;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.OffsetDateTime;
+import java.util.Locale;
+import java.util.stream.Stream;
+
+import static java.time.ZoneOffset.UTC;
 
 public class Main {
 
     private final static EmployeeParamDAO employeeDAO = new EmployeeParamDAO();
     private final static EmployeeAuditDAO employeeAuditDAO = new EmployeeAuditDAO();
+    private final static Faker faker = new Faker(Locale.of("pt", "BR"));
 
     public static void main(String[] args){
 
@@ -24,14 +32,16 @@ public class Main {
                 .load();
         flyway.migrate();
 
+        /*
         //INSERT
         var insert = new EmployeeEntity();
-        insert.setName("João'");
+        insert.setName("João");
         insert.setSalary(new BigDecimal("50000"));
         insert.setBirthday(OffsetDateTime.now().minusYears(20));
         System.out.println(insert);
         employeeDAO.insertWithProcedure(insert);
         System.out.println(insert);
+         */
 
 
         /*
@@ -61,6 +71,16 @@ public class Main {
         //employeeDAO.delete(insert.getId());
 
         //employeeAuditDAO.findAll().forEach(System.out::println);
+
+        var entities = Stream.generate(() -> {
+            var employee = new EmployeeEntity();
+            employee.setName(faker.name().fullName());
+            employee.setSalary(new BigDecimal(faker.number().digits(4)));
+            employee.setBirthday(OffsetDateTime.of(LocalDate.now().minusYears(faker.number().numberBetween(40, 20)), LocalTime.MIN, UTC));
+            return employee;
+        }).limit(10000).toList();
+
+        employeeDAO.insert(entities);
 
 
 
