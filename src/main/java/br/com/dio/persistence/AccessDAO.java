cf9769b7ba455792b448530eb77f9d6517ae88b9
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2026-09-21 19:22:04 -0300

    Create AccessDAO.java

---
+++ src/main/java/br/com/dio/persistence/AccessDAO.java
@@ -0,0 +1,29 @@
+package br.com.dio.persistence;
+
+import br.com.dio.persistence.entity.EmployeeEntity;
+import com.mysql.cj.jdbc.StatementImpl;
+
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.List;
+
+import static java.time.ZoneOffset.UTC;
+
+public class AccessDAO {
+
+    public void insert(final long employeeId, final long moduleId) {
+        try (
+                var connection = ConnectionUtil.getConnection();
+                var statement = connection.prepareStatement(
+                        "INSERT INTO accesses (employee_id, module_id) values (?, ?);"
+                )
+
+        ) {
+            statement.setLong(1, employeeId);
+            statement.setLong(2, moduleId);
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
